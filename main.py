1e689269de918b89186ca5cab682e198ca2b636f
Veryrasputin <118193726+NOGEORGIY@users.noreply.github.com>
2025-07-04 17:36:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-from fastapi import FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy import  Column, Integer, String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import sessionmaker
-from pydantic import BaseModel
-
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
 
-class User(BaseModel):
-    FIO: str
-    email: str
-    phoneNumber: str
-    role: str
 
-@app.post("/user/")
-async def create_item(user: User):
-    return user
+@app.get("/user")
+async def root():
+    return {"greeting": "User", "message": "Welcome to FastAPI!"}
